5e69b2f00053410713068988e1ed2617655f7499
xbb18175 <scott.clelland.2018@uni.strath.ac.uk>
2022-10-11 20:38:34 +0100

    changed posts page

--- pages/posts.js
+++ pages/posts.js
@@ -3,26 +3,26 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import UnmuteThumbnail from '../public/images/contents/Unmute-Thumbnail.png'
-import AfaanThumbnail from '../public/images/contents/Afaan-Thumbnail.png'
+import mernthumb from '../public/images/contents/mern-thumb.png'
+import learnthumb from '../public/images/contents/learn-thumb.png'
 
 const Posts = () => (
     <Layout title="Posts">
         <Container>
             <Heading as="h4" fontSize={20} mb={4}>
-                Popular Posts
+                Useful videos I have made use of
             </Heading>
             <Section delay={0.1}>
                 <SimpleGrid columns={[1,2,2]} gap={6}>
                     <GridItem 
-                        title="Twitch Highlights: I forgot to mute myself in Among Us..." 
-                        thumbnail={UnmuteThumbnail} 
-                        href="https://www.youtube.com/watch?v=pVRmvhUI-4M&t=2s"
+                        title="Full Stack MERN Project - Build and Deploy an App | React + Redux, Node, Express, MongoDB" 
+                        thumbnail={mernthumb} 
+                        href="https://www.youtube.com/watch?v=ngc9gnGgUdA&t=1659s"
                     />
                     <GridItem 
-                        title="Twitch Highlights: Warzone Funny Moments - Playing with the World's Best Teammate" 
-                        thumbnail={AfaanThumbnail} 
-                        href="https://www.youtube.com/watch?v=FWL9owhN4YY"
+                        title="Learn REACT JS in just 30 MINUTES (2021)" 
+                        thumbnail={learnthumb} 
+                        href="https://www.youtube.com/watch?v=vIRBSI-elUM"
                     />
                 </SimpleGrid>
             </Section>
